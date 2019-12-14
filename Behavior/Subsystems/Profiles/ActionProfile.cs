f7c5482cf1badebfd30b88234d50607b9231904f
Lucas Massey <lucas.massey@gmail.com>
2019-12-14 12:00:17 -0400

    Update ActionProfile.cs

--- Behavior/Subsystems/Profiles/ActionProfile.cs
+++ Behavior/Subsystems/Profiles/ActionProfile.cs
@@ -29,9 +29,32 @@ using RivalAI.Behavior.Settings;
 using RivalAI.Helpers;
 
 namespace RivalAI.Behavior.Subsystems.Profiles {
+    
     public class ActionProfile {
-
-
+        
+        public bool BroadcastMessage;
+        
+        public bool BarrelRoll;
+        
+        public bool ChangeAutopilotSpeed;
+        public float NewAutopilotSpeed;
+        
+        public bool SpawnEncounter;
+        public List<string> SpawnGroups;
+        
+        public ActionProfile(){
+        
+            BroadcastMessage = false;
+            
+            BarrelRoll = false;
+            
+            ChangeAutopilotSpeed = false;
+            NewAutopilotSpeed = 0;
+            
+            SpawnEncounter = false;
+            SpawnGroups = new List<string>();
+        
+        }
 
         public void InitTags(string customData) {
 
