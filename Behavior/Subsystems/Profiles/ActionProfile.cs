098d60739f5bbbd10dffcf1c6eed9367d85021b8
Lucas Massey <lucas.massey@gmail.com>
2019-12-14 14:49:42 -0400

    Update ActionProfile.cs

--- Behavior/Subsystems/Profiles/ActionProfile.cs
+++ Behavior/Subsystems/Profiles/ActionProfile.cs
@@ -30,17 +30,65 @@ using RivalAI.Helpers;
 
 namespace RivalAI.Behavior.Subsystems.Profiles {
     
+    [ProtoContract]
     public class ActionProfile {
         
+        [ProtoMember(1)]
         public bool BroadcastMessage;
         
+        [ProtoMember(2)]
         public bool BarrelRoll;
         
+        [ProtoMember(3)]
         public bool ChangeAutopilotSpeed;
+        
+        [ProtoMember(4)]
         public float NewAutopilotSpeed;
         
+        [ProtoMember(5)]
         public bool SpawnEncounter;
+        
+        [ProtoMember(6)]
         public List<string> SpawnGroups;
+        
+        [ProtoMember(7)]
+        public bool SelfDestruct;
+        
+        [ProtoMember(8)]
+        public bool Retreat;
+        
+        [ProtoMember(9)]
+        public bool SwitchToReceivedTarget;
+        
+        [ProtoMember(10)]
+        public bool SwitchToDamagerTarget;
+        
+        [ProtoMember(11)]
+        public bool RefreshTarget;
+        
+        [ProtoMember(12)]
+        public bool TriggerTimerBlocks;
+        
+        [ProtoMember(13)]
+        public List<string> TimeBlockNames;
+        
+        [ProtoMember(14)]
+        public bool ChangeReputationWithPlayers;
+        
+        [ProtoMember(15)]
+        public double ReputationChangeRadius;
+        
+        [ProtoMember(16)]
+        public int ReputationChangeAmount;
+        
+        [ProtoMember(17)]
+        public bool ActivateAssertiveAntennas;
+        
+        [ProtoMember(18)]
+        public bool ChangeAntennaOwnership;
+        
+        [ProtoMember(19)]
+        public string AntennaFactionOwner;
         
         public ActionProfile(){
         
@@ -53,12 +101,34 @@ namespace RivalAI.Behavior.Subsystems.Profiles {
             
             SpawnEncounter = false;
             SpawnGroups = new List<string>();
+            
+            SelfDestruct = false;
+            
+            Retreat = false;
+            
+            SwitchToReceivedTarget = false;
+            
+            SwitchToDamagerTarget = false;
+            
+            RefreshTarget = false;
+            
+            TriggerTimerBlocks = false;
+            TimeBlockNames = new List<string>();
+            
+            ChangeReputationWithPlayers = false;
+            ReputationChangeRadius = 0;
+            ReputationChangeAmount = 0;
+            
+            ActivateAssertiveAntennas = false;
+            
+            ChangeAntennaOwnership = false;
+            AntennaFactionOwner = "Nobody";
         
         }
 
         public void InitTags(string customData) {
 
-
+            
 
         }
 
