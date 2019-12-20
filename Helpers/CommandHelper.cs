add9eae24578a687105db4f238479bb65c6c9fe4
Lucas Massey <lucas.massey@gmail.com>
2019-12-20 10:09:41 -0400

    Update CommandHelper.cs

--- Helpers/CommandHelper.cs
+++ Helpers/CommandHelper.cs
@@ -1,1 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.Common;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Common.ObjectBuilders.Definitions;
+using Sandbox.Definitions;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.Game.EntityComponents;
+using Sandbox.Game.GameSystems;
+using Sandbox.Game.Weapons;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Weapons;
+using SpaceEngineers.Game.ModAPI;
+using ProtoBuf;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Utils;
+using VRageMath;
+using RivalAI;
+using RivalAI.Behavior;
+using RivalAI.Behavior.Subsystems;
+using RivalAI.Helpers;
 
+namespace RivalAI.Helpers {
+
+    public static class CommandHelper {
+
+      public static event Action<string, IMyRemoteControl, double, string, long> CommandTrigger;
+    
+    }
+    
+}
